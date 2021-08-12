0d44f94c4db11d281f425f9968acaf6ce1733c49
Guru <81556353+N3UR0MANC3R@users.noreply.github.com>
2021-08-12 11:44:31 +0400

    Add files via upload

---
+++ Cpl/chapter1/filecopying.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// 1.5.1 File copy
+
+int main()
+{
+    int c;
+
+    c = getchar();
+    while (c != EOF){
+        putchar(c);
+        c = getchar();
+    }
+}
+
+/* Read a char
+    while (char =/= EOF)
+        output char just read
+        read char
+*/
