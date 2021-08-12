4ddaf300d6b306b3dd02406199eb326e30da86ae
Guru <81556353+N3UR0MANC3R@users.noreply.github.com>
2021-08-12 10:38:33 +0400

    Add files via upload

---
+++ Cpl/chapter1/fahrcel.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//1.2 variable and arithmetic expressions
+
+int main()
+{
+    int fahr, celsius, kelvin;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32)/9;
+        kelvin = celsius + 273;
+        printf("%d\t%d\t%d\n", fahr, celsius, kelvin);
+        fahr = fahr + step;
+    }
+}
