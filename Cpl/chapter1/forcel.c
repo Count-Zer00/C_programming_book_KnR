dd61cb35605877a46838e51fa30e4731ec75f31b
Guru <81556353+N3UR0MANC3R@users.noreply.github.com>
2021-08-12 10:43:50 +0400

    Add files via upload

---
+++ Cpl/chapter1/forcel.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//1.3 The for statement.
+
+int main()
+{
+    int fahr;
+
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%3d %6.1f %6.1f\n",
+                fahr, (5.0/9.0)*(fahr-32), (fahr - 32)*5/9 + 273.15);
+}
---
+++ Cpl/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//hello world!
+
+int main()
+{
+    printf("Hello, World!\n");
+    printf("Peace be to all mankind!");
+    return 0;
+}
